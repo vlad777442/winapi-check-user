bb9a9098f90113b8a085f0000ffc54ae56db601e
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-02-12 17:18:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# winapi-check-user
---
+++ src/usersTable.js
@@ -0,0 +1,26 @@
+const
+    iconv = require('iconv-lite'),
+    { exec } = require('child_process')
+
+
+const exec_netUser = (callback) => {
+    exec('net user', { encoding: "buffer" },
+        (error, stdout, stderr) => {
+            if (error) {
+                callback(stderr, null)
+            } else {
+                callback(null, iconv.decode(stdout, 'CP866'))
+            }
+        }
+    );
+}
+
+
+const usersTable = (callback) => (exec_netUser((err, res) => {
+    callback(res
+        .slice(res.lastIndexOf('-') + 1)
+        .match(/([A-Za-zА-Яа-я0-9_]+)/g)
+    )
+}))
+
+module.exports = usersTable
